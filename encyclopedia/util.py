17c3b1522a3d644680380a72f8daa6937363a850
Andrew <andrewdfabrizio@gmail.com>
2020-09-27 22:48:29 -0400

    code smell updates from pycodestyle and pylint

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -10,7 +10,7 @@ def list_entries():
     """
     _, filenames = default_storage.listdir("entries")
     return list(sorted(re.sub(r"\.md$", "", filename)
-        for filename in filenames if filename.endswith(".md")))
+                for filename in filenames if filename.endswith(".md")))
 
 
 def save_entry(title, content):
@@ -18,38 +18,49 @@ def save_entry(title, content):
     Saves an encyclopedia entry, given its title and Markdown
     content. If an existing entry with the same title already exists,
     it is replaced.
+
+    Edited to check if entry with same name exists and provide
+    an appropriate response via dict.
     """
-# Edited to check if an entry with same name already exists and provide an appropriate response via dict.
     filename = f"entries/{title}.md"
     if default_storage.exists(filename):
         return {
             "entry": f' **ERROR:** A page already exists for **{title}.**',
             "name": "ERROR"
         }
-    else:
-        default_storage.save(filename, ContentFile(content))
-        return {
-            "entry": get_entry(title).get('content'),
-            "name": title
-        }
+    default_storage.save(filename, ContentFile(content))
+    return {
+        "entry": get_entry(title).get('content'),
+        "name": title
+    }
+
 
 def get_entry(title):
     """
     Retrieves an encyclopedia entry by its title. If no such
     entry exists, the function returns None.
+
+    Edited to return python dict including encyclopedia entry
+    and an 'ExactMatch' flag -which indicates whether or not
+    the search term found an exact match.
     """
-# Edited to return python dict including encyclopedia entry and an 'ExactMatch' flag -which indicates whether or not the search term found an exact match.
     try:
         content = default_storage.open(f"entries/{title}.md").read().decode("utf-8")
-        return {'content': content, 'name': title, 'ExactMatch': True}
+        return {'content': content,
+                'name': title,
+                'ExactMatch': True}
     except FileNotFoundError:
-        return {'content': "**ERROR:** The requested page was not found.", 'name': 'ERROR', 'ExactMatch': False}
+        return {'content': "**ERROR:** The requested page was not found.",
+                'name': 'ERROR',
+                'ExactMatch': False}
+
 
 def overwrite_entry(name, content):
-# Overwrites an existing entry with updated data submitted from edit.html.
+    """
+    Overwrites an existing entry with updated data submitted
+    from edit.html.
+    """
     filename = f"entries/{name}.md"
     if default_storage.exists(filename):
         default_storage.delete(filename)
         default_storage.save(filename, ContentFile(content))
-    else:
-        return 'File Not Found'
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,49 +1,71 @@
-from django.shortcuts import render
-from . import util
 import re
 import random
+from django.shortcuts import render
 import markdown2
+from . import util
 
 
 def index(request):
-# Renders the index.html page with listing of entries.
+    """
+    Renders the index.html page with listing of entries.
+    """
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
 def title(request, name):
-# Takes the name from the url string, invokes the Entry.html format, and requests the specific entry from a python dict returned by util.get_entry.
+    """
+    Takes the name from the url string, invokes the Entry.html format,
+    and requests the specific entry from a python dict returned
+    by util.get_entry.
+    """
     return render(request, "encyclopedia/Entry.html", {
         "entry": markdown2.markdown(util.get_entry(name).get('content')),
         "name": util.get_entry(name).get('name')
     })
 
+
 def search(request):
-# This function supports the search feature.  It determines if an exact match exists via an "ExactMatch" value in util.get_entry.
+    """
+    This function supports the search feature.  It determines if an exact match
+    exists via an "ExactMatch" value in util.get_entry.
+    """
     keyword = request.POST['q']
-    if util.get_entry(keyword).get('ExactMatch') == True:
-    # If an exact match exists, the function directs client to that entry.
+    if util.get_entry(keyword).get('ExactMatch') is True:
+        """
+        If an exact match exists, the function directs client to that entry.
+        """
         return render(request, "encyclopedia/entry.html", {
             "entry": markdown2.markdown(util.get_entry(keyword).get('content')),
             "name": keyword
         })
-    else:
-    # If client search query is not an exact match, results.html is rendered with a list of substring match results.
-        results = []
-        for entry in util.list_entries():
-            if re.search(keyword, entry, re.IGNORECASE):
-                results.append(entry)
-        return render(request, "encyclopedia/results.html", {
-            "results": results
-            })
+    """
+    If client search query is not an exact match, results.html is rendered with
+    a list of substring match results.
+    """
+    results = []
+    for entry in util.list_entries():
+        if re.search(keyword, entry, re.IGNORECASE):
+            results.append(entry)
+    return render(request, "encyclopedia/results.html", {
+        "results": results
+    })
+
 
 def create(request):
-# This function just catches a redirect from urls.py and renders the create.html page.
+    """
+    This function just catches a redirect from urls.py and renders
+    the create.html page.
+    """
     return render(request, "encyclopedia/create.html")
 
+
 def save(request):
-# Calls util.save_entry and renders the results in an Entry.html template.
-# util.save_entry runs the check to see if the entry already exists.
+    """
+    Calls util.save_entry and renders the results in an Entry.html template.
+    util.save_entry runs the check to see if the entry already exists.
+    """
     name = request.POST['title']
     submission = util.save_entry(name, request.POST['content'])
     return render(request, "encyclopedia/Entry.html", {
@@ -51,17 +73,24 @@ def save(request):
         "name": submission.get('name')
         })
 
+
 def edit(request):
-# Calls util.get_entry and prefills the entry onto the textarea at edit.html.
+    """
+    Calls util.get_entry and prefills the entry onto the textarea at edit.html.
+    """
     name = request.GET['name']
     return render(request, "encyclopedia/edit.html", {
         "entry": util.get_entry(name).get('content'),
         "name": name
     })
 
+
 def overwrite(request):
-# Calls util.overwrite_entry to save the client's updated data from edit.html over the existing file.
-# Next, it calls util.get_entry to serve the newly-updated entry content to the Entry.html template.
+    """
+    Calls util.overwrite_entry to save the client's updated data from edit.html
+    over the existing file. Next, it calls util.get_entry to serve the
+    newly-updated entry content to the Entry.html template.
+    """
     name = request.POST['name']
     content = request.POST['content']
     util.overwrite_entry(name, content)
@@ -70,9 +99,13 @@ def overwrite(request):
         "name": name
         })
 
+
 def random_page(request):
-# Selects a random entry from util.list_entries and serves the content to the entry.html template.
-    name= random.choice(util.list_entries())
+    """
+    Selects a random entry from util.list_entries and serves the content
+    to the entry.html template.
+    """
+    name = random.choice(util.list_entries())
     return render(request, "encyclopedia/Entry.html", {
         "entry": markdown2.markdown(util.get_entry(name).get('content')),
         "name": name
